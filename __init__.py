e49506c2cc93f6f12116906fef585539a8740455
Arthur Milchior <arthur@milchior.fr>
2020-11-23 02:47:00 +0100

    28+
    Original code from https://github.com/ijgnd/anki__batch_change_latex_header_footer/blob/master/src/__init__.py

--- __init__.py
+++ __init__.py
@@ -2,46 +2,53 @@
 #            Ankitects Pty Ltd and contributors
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/licenses/agpl.html
 
-import aqt
-from anki.lang import _
+
+from aqt import mw
 from aqt.models import Models
-from aqt.qt import *
-from aqt.utils import askUser, restoreGeom, saveGeom, tooltip
+from aqt.qt import (
+    QCheckBox,
+    QTabWidget,
+    QStackedWidget,
+    QWidget,
+    QVBoxLayout,
+)
+from aqt.utils import askUser
+from aqt.gui_hooks import (
+    models_advanced_will_show
+)
+from anki.hooks import wrap
 
 
 def changeLaTeX(latexsvg, header, footer):
-    for m in aqt.mw.col.models.all():
+    for m in mw.col.models.all():
         m['latexsvg'] = latexsvg
         m['latexPre'] = header
         m['latexPost'] = footer
-    aqt.mw.col.flush()
+        mw.col.models.save(m)
+
+
+def adjust_dialog(dialog):
+    global apply_to_all
+    apply_to_all = QCheckBox("Apply these settings to all models?")
+    # TODO use something nicer
+    for c in dialog.children():
+        if isinstance(c, QTabWidget):
+            for d in c.children():
+                if isinstance(d, QStackedWidget):
+                    for e in d.children():
+                        if isinstance(e, QWidget):
+                            for f in e.children():
+                                if isinstance(f, QVBoxLayout):
+                                    f.addWidget(apply_to_all)
+models_advanced_will_show.append(adjust_dialog)
 
 
 def onAdvanced(self):
-    d = QDialog(self)
-    frm = aqt.forms.modelopts.Ui_Dialog()
-    frm.setupUi(d)
-    # mod start
-    apply_to_all = QCheckBox("Apply these settings to all models?")
-    # must be run after setupUi since everything is defined in setupUi ...
-    frm.verticalLayout.addWidget(apply_to_all)
-    # mod end
-    frm.latexsvg.setChecked(self.model.get("latexsvg", False))
-    frm.latexHeader.setText(self.model['latexPre'])
-    frm.latexFooter.setText(self.model['latexPost'])
-    d.setWindowTitle(_("Options for %s") % self.model['name'])
-    frm.buttonBox.helpRequested.connect(lambda: openHelp("latex"))
-    restoreGeom(d, "modelopts")
-    d.exec_()
-    saveGeom(d, "modelopts")
-    self.model['latexsvg'] = frm.latexsvg.isChecked()
-    self.model['latexPre'] = str(frm.latexHeader.toPlainText())
-    self.model['latexPost'] = str(frm.latexFooter.toPlainText())
-    # mod start
+    global apply_to_all
     if apply_to_all.isChecked():
         if askUser("Write these values for the header/footer to ALL notetypes?"):
-            changeLaTeX(self.model['latexsvg'],
-                        self.model['latexPre'], self.model['latexPost'])
-
-
-Models.onAdvanced = onAdvanced
+            nt = self.current_notetype()
+            changeLaTeX(nt["latexsvg"],
+                        nt["latexPre"], 
+                        nt["latexPost"])
+Models.onAdvanced = wrap(Models.onAdvanced, onAdvanced)
